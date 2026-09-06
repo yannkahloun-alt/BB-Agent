5a0a339a59f93e86c29368febbbe5631ed622645
yannkahloun-alt <yann.kahloun@gmail.com>
2026-09-06 15:16:38 -0400

    Remove issue 57 kernel identity diagnostic

--- tests/test_issue57_kernel_identity_diagnostic.py
+++
@@ -1,8 +0,0 @@
-import json
-
-from bb_agent.live_ingest import current_live_kernel_identity
-
-
-def test_print_issue57_kernel_identity() -> None:
-    value = current_live_kernel_identity().to_wire_dict()
-    raise AssertionError(json.dumps(value, sort_keys=True, separators=(",", ":")))
