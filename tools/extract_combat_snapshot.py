e0d1f1c9641149d979cf25a01c1df40a05c3b3cb
yannkahloun-alt <yann.kahloun@gmail.com>
2026-09-07 01:21:07 -0400

    Mark intentional post-bootstrap extractor import

--- tools/extract_combat_snapshot.py
+++ tools/extract_combat_snapshot.py
@@ -13,7 +13,7 @@ SRC = ROOT / "src"
 if str(SRC) not in sys.path:
     sys.path.insert(0, str(SRC))
 
-from bb_agent.combat_sandbox import extract_latest_combat_sandbox
+from bb_agent.combat_sandbox import extract_latest_combat_sandbox  # noqa: E402
 
 _TEXT_RE = re.compile(r'<div class="text">(.*?)</div>', re.DOTALL)
 _TAG_RE = re.compile(r"<.*?>")
